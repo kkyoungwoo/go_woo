59a7d82ae45b22d80f11a3b0c5ecf45b58032e54
coder <bizdb@kakao.com>
2021-07-20 23:04:06 +0900

    portfolio_errer

--- src/components/component/portfolio/Carousel.jsx
+++ src/components/component/portfolio/Carousel.jsx
@@ -1,10 +1,107 @@
 import React from "react";
+import { useState,useCallback } from "react";
 import './carousel.css'
 
+const vestCarousel = [
+    {
+        text:"first",
+        img:"imgData",
+        skills:"사용기술 안내",
+        skills:"사용기술 안내1",
+        skillimg:"skill img",
+    },
+    {
+        text:"secound",
+        img:"imgData",
+        skills:"사용기술 안내2",
+        skillimg:"skill img",
+    },
+    {
+        text:"thred",
+        img:"imgData",
+        skills:"사용기술 안내3",
+        skillimg:"skill img",
+    },
+    {
+        text:"fourth",
+        img:"imgData",
+        skills:"사용기술 안내4",
+        skillimg:"skill img",
+    },
+    {
+        text:"five",
+        img:"imgData",
+        skills:"사용기술 안내5",
+        skillimg:"skill img",
+    },
+    {
+        text:"six",
+        img:"imgData imgData imgData",
+        skills:"사용기술 안내6",
+        skillimg:"skill img",
+    },
+    {
+        text:"sever",
+        img:"imgData",
+        skills:"사용기술 안내7",
+        skillimg:"skill img",
+    },
+    {
+        text:"eight",
+        img:"imgData",
+        skills:"사용기술 안내8",
+        skillimg:"skill img",
+    },
+    {
+        text:"nine",
+        img:"imgData",
+        skills:"사용기술 안내9",
+        skillimg:"skill img",
+    },
+    {
+        text:"ten",
+        img:"imgData",
+        skills:"사용기술 안내10",
+        skillimg:"skill img",
+    },
+]
+
 export default function Carousel(){
+
+    const [displayControl,setDisplayControl] = useState("displaynone")
+
+    const potfoiloData = useCallback((e)=>{
+        if(displayControl === "displaynone"){
+            setDisplayControl("block")
+        }else if(displayControl === "block"){
+            setDisplayControl("displaynone")
+        }
+    },[displayControl])
+
     return(
         <div>
-            베스트 캐러셀 영역
+            <div className="Carousel_wrap">
+                {vestCarousel.map((item,idx)=>{
+                    return(
+                        <div className="Carousel_box" key={idx} onClick={(e)=>potfoiloData(e)}>
+                            <div>{item.img}</div>
+                        </div>
+                    )
+                })}
+            </div>
+            <div className="carousel_title">
+                사용한 기능을 안내합니다.
+            </div>
+            <div className="Carousel_inpo">
+                {vestCarousel.map((item,idx)=>{
+                    return(
+                        <div className={"Carousel_box " + displayControl} key={idx} >
+                            <div>{item.skills}</div>
+                            <div>{item.skillimg}</div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- src/components/component/portfolio/Portfolio.jsx
+++ src/components/component/portfolio/Portfolio.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import './portfolio.css'
 
 import Carousel from './Carousel'
-import CarouselScroll from './CarouselScroll'
 
 export default function Portfolio(){
     return(
@@ -12,12 +11,6 @@ export default function Portfolio(){
                     <div className="portfolio_warp">
                         <div className="carousel">
                             <Carousel />
-                        </div>
-                        <div className="carousel_title">
-                            사용 기능한 기능을 안내합니다.
-                        </div>
-                        <div className="carousel_scroll">
-                            <CarouselScroll />
                         </div>
                     </div>
                 </div>
