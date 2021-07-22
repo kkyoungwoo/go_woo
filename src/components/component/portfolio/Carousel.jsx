d0924cd9009487b22078c55bbc74d56f8a88709c
coder <bizdb@kakao.com>
2021-07-22 22:56:58 +0900

    7.22(11:00)
    7.22(11:00)

--- src/components/component/portfolio/Carousel.jsx
+++ src/components/component/portfolio/Carousel.jsx
@@ -9,68 +9,72 @@ const vestCarousel = [
         skills:"사용기술 안내",
         skills:"사용기술 안내1",
         skillimg:"skill img",
+        size:"80",
     },
     {
         text:"secound",
         img:"imgData",
         skills:"사용기술 안내2",
         skillimg:"skill img",
+        size:"60",
     },
     {
         text:"thred",
         img:"imgData",
         skills:"사용기술 안내3",
         skillimg:"skill img",
+        size:"40",
     },
     {
         text:"fourth",
         img:"imgData",
         skills:"사용기술 안내4",
         skillimg:"skill img",
+        size:"20",
     },
     {
         text:"five",
         img:"imgData",
         skills:"사용기술 안내5",
         skillimg:"skill img",
+        size:"0",
     },
     {
         text:"six",
         img:"imgData imgData imgData",
         skills:"사용기술 안내6",
         skillimg:"skill img",
+        size:"-20",
     },
     {
         text:"sever",
         img:"imgData",
         skills:"사용기술 안내7",
         skillimg:"skill img",
+        size:"-40",
     },
     {
         text:"eight",
         img:"imgData",
         skills:"사용기술 안내8",
         skillimg:"skill img",
+        size:"-60",
     },
     {
         text:"nine",
         img:"imgData",
         skills:"사용기술 안내9",
         skillimg:"skill img",
-    },
-    {
-        text:"ten",
-        img:"imgData",
-        skills:"사용기술 안내10",
-        skillimg:"skill img",
+        size:"-80",
     },
 ]
+
 
 export default function Carousel(){
 
     const [displayControl,setDisplayControl] = useState("displaynone")
-
     const potfoiloData = useCallback((e)=>{
+
         if(displayControl === "displaynone"){
             setDisplayControl("block")
         }else if(displayControl === "block"){
@@ -78,16 +82,34 @@ export default function Carousel(){
         }
     },[displayControl])
 
+
+    const [currentIdx, setCurrentIdx] = useState(Math.floor(vestCarousel.length / 2));
+
+
+    const hendlePrev = () => {
+        setCurrentIdx((prev) => (prev + (vestCarousel.length - 1)) % vestCarousel.length)
+    }
+
+    const hendleNext = () => {
+        setCurrentIdx((next) => (next + (vestCarousel.length + 1)) % vestCarousel.length)
+    }
+
     return(
         <div>
             <div className="Carousel_wrap">
+                <div className="prev_btn" onClick={hendlePrev}>◀</div>
                 {vestCarousel.map((item,idx)=>{
                     return(
-                        <div className="Carousel_box" key={idx} onClick={(e)=>potfoiloData(e)}>
+                        <div className="Carousel_box" key={idx} onClick={(e)=>potfoiloData(e)}
+                            style={{
+                            transform:`translateX(${vestCarousel[currentIdx].size/3}rem)`,
+                        }}
+                        >
                             <div>{item.img}</div>
                         </div>
                     )
                 })}
+                <div className="next_btn" onClick={hendleNext}>▶</div>
             </div>
             <div className="carousel_title">
                 사용한 기능을 안내합니다.
@@ -95,9 +117,10 @@ export default function Carousel(){
             <div className="Carousel_inpo">
                 {vestCarousel.map((item,idx)=>{
                     return(
-                        <div className={"Carousel_box " + displayControl} key={idx} >
+                        <div className={"Carousel_box " + displayControl} key={idx}>
                             <div>{item.skills}</div>
                             <div>{item.skillimg}</div>
+                            {/* filter 함수 더 알아보고 필터링 해보기*/}
                         </div>
                     )
                 })}
